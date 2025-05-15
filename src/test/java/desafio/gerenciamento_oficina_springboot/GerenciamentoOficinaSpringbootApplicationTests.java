6430e459e292c523ac099664cebfa65f7327a037
Lucas Oliveira <lo8304179@gmail.com>
2025-05-15 19:01:33 -0300

    first commit

---
+++ src/test/java/desafio/gerenciamento_oficina_springboot/GerenciamentoOficinaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.gerenciamento_oficina_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoOficinaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
